56f4489bb06ec7939064016b2eb2b840b1d79533
Elsever <106514920+Elsever-Emirli@users.noreply.github.com>
2023-01-10 16:57:41 +0300

    blgs

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -16,13 +16,12 @@ const Blog = (props: Props) => {
   return (
     <div className='w-full h-full flex items-center  flex-col  text-white '>{
       props.post.map((post:Data,key:any)=>(
-        <div  className='' key={key}>
+        <div  className='text-center my-3 ' key={key}>
           <h2 className='' >{post.name}</h2>
           <p>{post.explanation}</p>
         </div>
       ))
     }
-    <Link href='addpost'><p className='text-red'>Add Post</p></Link>
     </div>
   )
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import NavBar from '../components/NavBar'
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <NavBar />
+    
+    <NavBar>
     <Component {...pageProps} />
+    </NavBar>
+    
     </>
     
     
