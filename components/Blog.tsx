20af722de0a2587c8dc321843ee252d8e45729c4
Elsever <106514920+Elsever-Emirli@users.noreply.github.com>
2023-01-18 20:28:33 +0300

    flex wrap

--- components/Blog.tsx
+++ components/Blog.tsx
@@ -17,12 +17,12 @@ const Blog = (props: Props) => {
   console.log();
 
   return (
-    <div className='w-full h-full flex items-center  text-white  italic  disable-select  '>{
+    <div className=' h-full flex items-center flex-wrap justify-center  text-white  italic  disable-select  '>{
       props.post.map((post:Data,key:any)=>(
         <Link href={`/post/${post._id}`} key={key}>
         <div  className='text-center my-3 w-52 hover:text-cyan-400 mx-2 ' key={key}>
           <h2 className='' >{post.name}</h2>
-          <p>{(post.explanation.length >100) ?  post.explanation.slice(0,99)+'....' : post.explanation }</p>
+          <p>{(post.explanation?.length >100) ?  post.explanation.slice(0,99)+'....' : post.explanation }</p>
           <h6>{post.date}</h6>
         </div>
         </Link>
