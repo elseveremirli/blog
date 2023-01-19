774a3b9182bf4627b9649f28e735026e1c00c299
Elsever <106514920+Elsever-Emirli@users.noreply.github.com>
2023-01-19 20:06:31 +0300

    go

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -45,18 +45,18 @@ const NavBar = (props: Props) => {
     </div>
 
 
-    <div className='row-start-2 row-end-13 overflow-y-auto  '>
+    <div className='row-start-2 row-end-11 overflow-y-auto  '>
     {
       props.children
     }
     </div>
     
 
-    <footer className="bg-gray-900 mb-0 row-start-13 row-end-14 disable-select md:mb-5 ss:mb-5">
+    <footer className="bg-gray-900 mb-0 row-start-11 row-end-13 disable-select">
         <div className="px-4 py-6 bg-gray-900 md:flex md:items-center md:justify-between">
             <span className="text-sm text-gray-500  sm:text-center">© 2023 <a href="https://www.instagram.com/the.horror.st/">Elsever's™</a>. All Rights Reserved.
             </span>
-            <div className="flex mt-4 space-x-6 sm:justify-center md:mt-0  xs:hidden  ">
+            <div className="flex mt-4 space-x-6 sm:justify-center md:mt-0">
                 <a href="https://www.facebook.com/elsever.emirli.33/" className="text-gray-400 hover:text-gray-900 dark:hover:text-white">
                     <svg className="w-5 h-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" /></svg>
                     <span className="sr-only">Facebook page</span>
